09ef5e625380edafeb5ef1567094a7153294b0be
dvaquerizoenc <davidvaquerizoenc@gmail.com>
2024-11-06 17:46:48 +0100

    Update Cola.java

--- PSP_02_ColaDeCorreo/src/modelo/negocio/Cola.java
+++ PSP_02_ColaDeCorreo/src/modelo/negocio/Cola.java
@@ -1,5 +1,25 @@
 package modelo.negocio;
 
-public class Cola {
+import java.util.LinkedList;
+import java.util.Queue;
 
+import modelo.entidad.Email;
+
+public class Cola {
+	public final static int MAX_ELEMENTS = 5;
+	
+	private Queue<Email> cola = new LinkedList<>();
+	
+	public synchronized void addEmail(Email email) {
+		while(cola.size() == MAX_ELEMENTS) {
+			try {
+				wait();
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		cola.offer(email);
+		notify();
+	}
 }
